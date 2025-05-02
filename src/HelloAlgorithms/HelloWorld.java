419b2ede1b40d10753cd2694bc4ac994a5188c75
Nikolay Angelov <n.angelov90@abv.bg>
2025-05-02 14:25:32 +0300

    Add initial algorithms exercises and algs4 library setup
    Introduce three classes: HelloWorld, HelloGoodbye, and RandomWord for basic algorithmic exercises.

---
+++ src/HelloAlgorithms/HelloWorld.java
@@ -0,0 +1,10 @@
+package HelloAlgorithms;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        StdOut.println("Hello, World");
+    }
+}
